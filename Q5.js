cd523580547d136948d0965797e6a7bfe0a84009
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-01 17:06:35 +0500

    Add files via upload
    Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ Q5.js
@@ -0,0 +1,4 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Thomas Edison";
+var message = famous_person + 'once said "Genius is one percent inspiration and ninety-nine percent perspiration"';
+console.log(message);
---
+++ Q5.ts
@@ -0,0 +1,5 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person : string  = "Thomas Edison";
+let message: string = famous_person +'once said "Genius is one percent inspiration and ninety-nine percent perspiration"';
+console.log(message);
