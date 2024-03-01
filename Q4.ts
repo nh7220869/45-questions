150e01d0948b4286a28b7f2bb117d6eacd710967
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-01 17:05:35 +0500

    Add files via upload
    Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:

---
+++ Q4.ts
@@ -0,0 +1,2 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks
+console.log('Thomas Edison  once said, "Genius is one percent inspiration and ninety-nine percent perspiration"');
