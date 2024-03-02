d988fc92e4b62ff69a5310f046738a9c99368adc
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-02 19:33:30 +0500

    Add files via upload
    Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ Q6.ts
@@ -0,0 +1,8 @@
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces    
+
+let name1 :string = "\n\t sehar \n\t"
+console.log(name1)
+
+let name2 :string = "sehar"
+console.log(name2)
+
