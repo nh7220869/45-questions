44ba5bdf0b0ada3d530a75b4683e5735eb9aa030
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-03 00:48:04 +0500

    Add files via upload
    Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Q14.js
@@ -0,0 +1,5 @@
+var personname = ['SEHAR,', 'ABUBAKAR,', 'ABDULLAH,'];
+var invitation = "please come hear on dinner with us.";
+console.log(personname[0], invitation);
+console.log(personname[1], invitation);
+console.log(personname[2], invitation);
---
+++ Q14.ts
@@ -0,0 +1,6 @@
+let personname: string[] = ['SEHAR,','ABUBAKAR,','ABDULLAH,']
+
+let invitation = "please come hear on dinner with us."
+console.log(personname[0] ,invitation)
+console.log(personname [1],invitation)
+console.log(personname [2],invitation)
