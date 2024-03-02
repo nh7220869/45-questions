33ea26cd094b350458045f91e9b987473be6789e
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-02 22:11:26 +0500

    Add files via upload
    Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ Q9.js
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoritenumber = 8.;
+var messege = "My luckiest number is";
+console.log(messege, favoritenumber);
---
+++ Q9.ts
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+let favoritenumber = 8.
+let messege ="My luckiest number is"
+console.log(messege, favoritenumber);
