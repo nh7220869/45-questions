206df89b8767443b416e95d9ee848659d80d8489
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-02 23:47:42 +0500

    Add files via upload
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Q12.ts
@@ -0,0 +1,8 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let name1 = ['Hello Noor ul Sehar!', 'Hello Noor ul Huda!' , 'Hello Syeda Rimsha!' , 'Hello Eshaal fatima'];
+let messege = "I hope you are having a fantastic day.  "
+console.log(name1[0] , messege);
+console.log(name1[1] , messege);
+console.log(name1[2] , messege);
+console.log(name1[3] , messege);
