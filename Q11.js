428b5811f4f8545d006bf494ea97bcb4f9901638
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-02 23:41:13 +0500

    Add files via upload
    Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.

---
+++ Q11.js
@@ -0,0 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var name1 = ['Noor ul Sehar', 'Noor ul Huda', 'Syeda Rimsha', 'Eshal Fatima'];
+console.log(name1[0]);
+console.log(name1[1]);
+console.log(name1[2]);
+console.log(name1[3]);
---
+++ Q11.ts
@@ -0,0 +1,7 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let name1 : string [] = ['Noor ul Sehar', 'Noor ul Huda', 'Syeda Rimsha', 'Eshal Fatima'];
+console.log(name1[0]);
+console.log(name1[1]);
+console.log(name1[2]);
+console.log(name1[3]);
