16f04ec7306f5baef8fda49134922378acb40a7e
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-03 00:26:28 +0500

    Add files via upload
    Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”

---
+++ Q13.js
@@ -0,0 +1,6 @@
+var name1 = ['REVO.', 'civic.', 'land cruiser.', 'FORD.'];
+var message = 'I want to own a';
+console.log(message, name1[0]);
+console.log(message, name1[1]);
+console.log(message, name1[2]);
+console.log(message, name1[3]);
---
+++ Q13.ts
@@ -0,0 +1,6 @@
+let name1: string [] = ['REVO.','civic.','land cruiser.','FORD.']
+let message = 'I want to own a'
+console.log( message, name1[0])
+console.log( message, name1[1])
+console.log( message, name1[2])
+console.log( message, name1[3])
