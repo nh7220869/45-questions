a5ceab57442cad5b663c3d8b2837256cb87b80c7
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-02 20:24:55 +0500

    Add files via upload
    Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ Q7.js
@@ -0,0 +1,17 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//Addtion
+var num1 = 4;
+var num2 = 4;
+console.log(num1 + num2);
+//multiplication
+var num3 = 4;
+var num4 = 2;
+console.log(num3 * num4);
+//subtrition
+var num5 = 20;
+var num6 = 12;
+console.log(num5 - num6);
+//division
+var num7 = 32;
+var num8 = 4;
+console.log(num7 / num8);
---
+++ Q7.ts
@@ -0,0 +1,21 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//Addtion
+let num1 = 4
+let num2 = 4
+console.log(num1+num2)
+
+//multiplication
+let num3 = 4
+let num4 = 2 
+console.log(num3*num4)
+
+//subtrition
+let num5 = 20
+let num6 = 12
+console.log(num5-num6)
+
+//division
+let num7 = 32
+let num8 = 4
+console.log(num7/num8)
