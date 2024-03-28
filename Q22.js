3077b617422989336a1c7abfda8f5ffe2ab20970
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-28 22:19:36 +0500

    Add files via upload
    Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ Q22.js
@@ -0,0 +1,6 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+var friend = ["Abubakar", "Abdullah", "Sehar", "Huda"];
+console.log(friend[6]);
+//index error solve
+var friends = ["Abubakar", "Abdullah", "Sehar", "Huda", "Eshal", "Ahad", "Fatima",];
+console.log(friends[6]);
---
+++ Q22.ts
@@ -0,0 +1,13 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
+let friend : string[]= ["Abubakar", "Abdullah", "Sehar", "Huda"];
+console.log(friend[6]);
+
+//index error solve
+
+
+let friends : string[]= ["Abubakar", "Abdullah", "Sehar", "Huda", "Eshal", "Ahad", "Fatima",];
+console.log(friends[6]);
+
+
