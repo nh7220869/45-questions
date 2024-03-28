918745c20539420f608f1560eb855451d08f5a20
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-28 21:02:37 +0500

    Add files via upload
    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ Q20.ts
@@ -0,0 +1,9 @@
+// Task 20
+//hink of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries,
+// cities, languages, or anything else you’d like.
+// Write a program that creates a list containing these items.
+let countries: string[] = ['Turkey','Egypt','Bangladash','Iran'];
+
+
+console.log("Countries I'd like to visit:", countries );
