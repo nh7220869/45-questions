7b72885a3b34ec656e64c4e5fe559bf4caf1c8bb
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-28 23:32:45 +0500

    Add files via upload
    Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:

---
+++ Q23.js
@@ -0,0 +1,38 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+//condition is a type of boolen which one is the type of data
+var car = 'subaru';
+//Test1: Equality comparasion
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//Test2 : strict equality comparasion
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+//Test3 : Inequality comparasion
+console.log("Is car != 'subaru'? I predict False.");
+console.log(car != 'subaru');
+//Test4 : strict inequality comparasion
+console.log("Is car !== 'subaru'? I predict False.");
+console.log(car !== 'subaru');
+//Test5 : less than comparasion
+console.log("Is car < 'subaru'? I predict True.");
+console.log(car < 'subaru');
+//Test6 : greater than comparasion
+console.log("Is car > 'subaru'? I predict True.");
+console.log(car > 'subaru');
+//Test7 : less than or equal comparasion 
+console.log("Is car <= 'subaru'? I predict True.");
+console.log(car <= 'subaru');
+//Test8: greater than or equal comparasion 
+console.log("Is car >= 'subaru'? I predict False.");
+console.log(car >= 'subaru');
+//Test9 : check truth
+console.log("Is car == 'toyota'? I predict True.");
+console.log(car == 'toyota');
+//Test10 : check false
+console.log("Is car != 'toyota'? I predict False.");
+console.log(car != 'toyota');
