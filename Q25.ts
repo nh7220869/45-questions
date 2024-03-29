f7e0511270a32012329514cc66d291fea0fd96d2
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-29 22:39:31 +0500

    Add files via upload
    Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
    • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
    
    • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)

---
+++ Q25.ts
@@ -0,0 +1,22 @@
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+//• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+
+console.log("statment:")
+
+let color1:string="Red"
+ if( color1 =='Red'){
+    console.log('play just earn 5 point')
+ }else{
+    
+ }
+
+console.log('no output:')
+ let color2="Red"
+ if( color2 =='yellow'){
+    console.log('play just earn 5 point')
+ }else{
+    
+ }
