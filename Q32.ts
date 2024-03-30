3b0b8042c66fc6b0c357b969cb4ddd2f96973f4c
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-03-30 16:26:34 +0500

    Add files via upload
    Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
    • Make a list of five or more usernames called current_users.
    
    • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
    
    • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
    
    • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
    
    Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
    • Store the numbers 1 through 9 in a array.
    
    • Loop through the array.
    
    • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.

---
+++ Q32.ts
@@ -0,0 +1,20 @@
+/*Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+
+• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+
+• Loop through the new_users list to see if each new username has already been used. If it has, print a message that  pertheson will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+
+• Make sure your comparison is case insensitive. If 'John' has been used'JOHN' should not be accepted.*/
+
+let username:string[] =['Admin','ali','David','ahmed']
+let newusername:string[] =['muhammad','abdullah','david','ahmed','umer','usam']
+
+newusername.forEach(newusername=>{
+    let lowercase=newusername.toLowerCase()
+   if(username.map(user=>user.toLowerCase()).includes(lowercase)){
+   console.log(`the username ${newusername} is not availabal.plaes write a different username`)
+}else{
+    console.log(`The user ${newusername} is available`)
+}
+})
