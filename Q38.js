ccf1f2e5407342b0455b24e73c69a678f9864890
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-04-02 22:36:20 +0500

    Add files via upload
    Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.

---
+++ Q38.js
@@ -0,0 +1,7 @@
+/*Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.*/
+function describe_city(city, country) {
+    console.log("The ".concat(city, " is in ").concat(country));
+}
+describe_city('karachi', 'Pakistan');
+describe_city('londan', 'England');
+describe_city('Dehli', 'India');
